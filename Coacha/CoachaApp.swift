8eacea7aad0f846b26cf629fba40d48c375d0d85
Antonín Šimek <ansimek@csas.cz>
2022-03-04 14:42:06 +0100

    Initial Commit

---
+++ Coacha/CoachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachaApp.swift
+//  Coacha
+//
+//  Created by Anthony Šimek on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coacha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coacha
+//
+//  Created by Anthony Šimek on 04.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
